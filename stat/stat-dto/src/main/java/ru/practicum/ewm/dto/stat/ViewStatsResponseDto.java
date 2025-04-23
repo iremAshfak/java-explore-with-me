b5c898887c4a01d1563e0e1a91888018fc26a529
Irem <irem.ashfak@gmail.com>
2025-04-24 01:44:25 +0300

    fix stat-server

--- stat/stat-dto/src/main/java/ru/practicum/ewm/dto/stat/ViewStatsResponseDto.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.ewm.dto.stat;
-
-public record ViewStatsResponseDto(
-        String app,
-        String uri,
-        Long hits
-) {
-}
--- stat/stat-server/src/main/java/ru/practicum/ewm/server/stat/ConstantDate.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.ewm.server.stat;
-
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class ConstantDate {
-    public static final String DATE = "yyyy-MM-dd HH:mm:ss";
-}
