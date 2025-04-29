030d056cf7e371dd0b76991a888bf3b1e75f6887
Irem <irem.ashfak@gmail.com>
2025-04-29 17:48:31 +0300

    main

---
+++ main-service/src/main/java/ru/practicum/ewm/dto/response/UserResponseDto.java
@@ -0,0 +1,18 @@
+package ru.practicum.ewm.dto.response;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
+
+public record UserResponseDto(
+        long id,
+        @NotBlank
+        @Length(min = 6, max = 254)
+        String email,
+        @NotBlank
+        @Size(min = 2)
+        @Size(max = 250)
+        String name
+) {
+}
