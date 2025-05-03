8b93ba2d2be8d017f3b4e255d1cbd3ce082df42b
Irem <irem.ashfak@gmail.com>
2025-05-03 16:02:10 +0300

    fixes

--- main-service/src/main/java/ru/practicum/ewm/dto/response/CommentResponseDto.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/response/CommentResponseDto.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import ru.practicum.ewm.dto.info.UserInfoDto;
 
 public record CommentResponseDto(
-        Long id,
         @NotBlank
         String text,
         String created,
