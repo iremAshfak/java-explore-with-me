753d1b30c589b3dda3cc90aee96dc7fb5f309201
Irem <irem.ashfak@gmail.com>
2025-05-03 20:58:20 +0300

    fixes

--- main-service/src/main/java/ru/practicum/ewm/dto/create/CommentCreateDto.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/create/CommentCreateDto.java
@@ -1,11 +1,13 @@
 package ru.practicum.ewm.dto.create;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CommentCreateDto(
         @NotBlank
         String text,
         String created,
+        @NotNull
         Long commenterId,
         Long eventId
 ) {
