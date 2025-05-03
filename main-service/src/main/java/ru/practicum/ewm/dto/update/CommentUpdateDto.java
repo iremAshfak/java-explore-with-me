1ea2aab11dfaf1a3f0c096f0ba775ceb01aca31d
Irem <irem.ashfak@gmail.com>
2025-05-03 21:14:27 +0300

    fixes

--- main-service/src/main/java/ru/practicum/ewm/dto/update/CommentUpdateDto.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/update/CommentUpdateDto.java
@@ -1,12 +1,14 @@
 package ru.practicum.ewm.dto.update;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CommentUpdateDto(
         @NotBlank
         String text,
         String created,
         Long updatedById,
+        @NotNull
         Long eventId
 ) {
 }
