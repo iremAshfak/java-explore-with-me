8470a9f0d899226dde2417a930aca65eda2d25c0
Irem <irem.ashfak@gmail.com>
2025-05-03 19:57:45 +0300

    fixes

--- main-service/src/main/java/ru/practicum/ewm/dto/response/CommentResponseDto.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/response/CommentResponseDto.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import ru.practicum.ewm.dto.info.UserInfoDto;
 
 public record CommentResponseDto(
-        long id,
+        Long id,
         @NotBlank
         String text,
         String created,
--- main-service/src/main/java/ru/practicum/ewm/dto/update/CommentUpdateDto.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/update/CommentUpdateDto.java
@@ -3,6 +3,7 @@ package ru.practicum.ewm.dto.update;
 import jakarta.validation.constraints.NotBlank;
 
 public record CommentUpdateDto(
+        Long id,
         @NotBlank
         String text,
         String created,
