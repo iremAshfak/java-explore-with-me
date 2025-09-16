377dd8bc2f67cf3da8d0abf244b94d1afd3585e0
Irem Ashfak <iashfak@yandex.ru>
2025-09-16 22:50:39 +0300

    Update CommentUpdateDto.java

--- main-service/src/main/java/ru/practicum/ewm/dto/update/CommentUpdateDto.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/update/CommentUpdateDto.java
@@ -1,7 +1,9 @@
 package ru.practicum.ewm.dto.update;
 
+import jakarta.validation.constraints.NotBlank;
 
 public record CommentUpdateDto(
+        @NotBlank
         String text,
         String created,
         Long updatedById,
